14f6d19a779c84ea7a6695ef072b2b2e9e8c9352
Pontus Johansson <pontus.johansson@dse.se>
2013-09-27 10:28:13 +0200

    Commit from bash

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Pontus
+ * Date: 27/09/13
+ * Time: 09:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello!");
+    }
+}
